524ad21074b097f4ed20c3e1b5f145c116e7e6a2
Mathangi Pushparajah <mathangi23@outlook.com>
2024-05-27 22:31:09 +0200

    Initial commit

---
+++ src/snake_game/GamePanel.java
@@ -0,0 +1,12 @@
+package snake_game;
+
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
