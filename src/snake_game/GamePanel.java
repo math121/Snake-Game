994318156f464d7a8639d133eea8606e370e4759
Mathangi Pushparajah <mathangi23@outlook.com>
2024-05-27 23:37:50 +0200

    Make a grid for the game

--- src/snake_game/GamePanel.java
+++ src/snake_game/GamePanel.java
@@ -1,10 +1,62 @@
 package snake_game;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    private static final int SCREEN_WIDTH = 700;
+    private static final int SCREEN_HEIGHT = 700;
+    private static final int UNIT_SIZE = 30;
+    private static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+    private static final int DELAY = 70;
+    private final int[] x = new int[GAME_UNITS];
+    private final int[] y = new int[GAME_UNITS];
+
+    private int snakeBodyParts = 3;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R'; // default - right
+
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    public GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        //this.setFocusable(false);
+        this.addKeyListener(new MyKeyAdapter());
+        this.startGame();
+    }
+
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        draw(graphics);
+    }
+
+    public void draw(Graphics graphics) {
+        for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+            graphics.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            graphics.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+
+        }
+    }
+
+    public void newApple() {}
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
---
+++ src/snake_game/MyKeyAdapter.java
@@ -0,0 +1,11 @@
+package snake_game;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class MyKeyAdapter extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e) {
+        super.keyPressed(e);
+    }
+}
